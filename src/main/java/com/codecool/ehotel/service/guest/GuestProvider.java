e1e07820dfd5d0ed54d05c16ca42dc2a12135529
Marton Benjamin Szak <105061239+martonszak@users.noreply.github.com>
2022-11-07 15:05:58 +0100

    complete "Guest for a day" and "Generate guests" tasks.

---
+++ src/main/java/com/codecool/ehotel/service/guest/GuestProvider.java
@@ -0,0 +1,56 @@
+package com.codecool.ehotel.service.guest;
+
+import com.codecool.ehotel.model.Guest;
+import com.codecool.ehotel.model.GuestType;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class GuestProvider implements GuestService {
+
+    private final List<String> firstNames = List.of("Thomas", "Martin", "Robert", "Feodora", "Rick", "Sequoia", "Mary", "Jennifer", "Karen", "Sarah");
+
+    private final List<String> lastnames = List.of("Smith", "Wader", "Miller", "Wild", "White", "Hill", "Perez", "Quagmire", "Griffin", "Sandler");
+
+    private String generateRandomGuestName(List<String> firstNames, List<String> lastnames) {
+        Random random = new Random();
+        String firstName = firstNames.get(random.nextInt(firstNames.size()));
+        String lastName = lastnames.get(random.nextInt(lastnames.size()));
+        String randomName = firstName + " " + lastName;
+        return randomName;
+    }
+
+    private LocalDate randomCheckInOrOutDate(LocalDate seasonStart, LocalDate seasonEnd) {
+        long seasonStartEpochDay = seasonStart.toEpochDay();
+        long seasonEndEpochDay = seasonEnd.toEpochDay();
+        long randomDay = ThreadLocalRandom.current().nextLong(seasonStartEpochDay, seasonEndEpochDay);
+        return LocalDate.ofEpochDay(randomDay);
+    }
+
+    @Override
+    public Guest generateRandomGuest(LocalDate seasonStart, LocalDate seasonEnd) {
+        Long daysBetween = DAYS.between(seasonStart, seasonEnd);
+        LocalDate randomCheckInDate = randomCheckInOrOutDate(seasonStart, seasonEnd);
+        LocalDate randomMaxCheckOutDate = randomCheckInDate.plusDays(daysBetween >= 7 ? 7 : daysBetween);
+        LocalDate randomCheckOutDate = randomCheckInOrOutDate(randomCheckInDate, randomMaxCheckOutDate);
+        Guest newRandomGuest = new Guest(generateRandomGuestName(firstNames, lastnames), GuestType.randomGuestType(), randomCheckInDate, randomCheckOutDate);
+        return newRandomGuest;
+    }
+
+    @Override
+    public Set<Guest> getGuestsForDay(List<Guest> guests, LocalDate date) {
+        Set<Guest> guestsOnGivenDate = new HashSet<>();
+        for (Guest guest : guests) {
+            if (!date.isBefore(guest.checkIn()) && !date.isAfter(guest.checkOut())) {
+                guestsOnGivenDate.add(guest);
+            }
+        }
+        return guestsOnGivenDate;
+    }
+}
