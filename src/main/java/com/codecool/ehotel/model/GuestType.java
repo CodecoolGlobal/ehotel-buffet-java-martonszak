524ec9a71655c80f6040f9cec5bb2b5c4838abf5
Marton Benjamin Szak <105061239+martonszak@users.noreply.github.com>
2022-11-07 19:06:25 +0100

    complete "Buffet state" and "Buffet refill" tasks.

--- src/main/java/com/codecool/ehotel/model/GuestType.java
+++ src/main/java/com/codecool/ehotel/model/GuestType.java
@@ -6,11 +6,9 @@ import java.util.Random;
 import static com.codecool.ehotel.model.MealType.*;
 
 public enum GuestType {
-
     BUSINESS(List.of(SCRAMBLED_EGGS, FRIED_BACON, CROISSANT)),
     TOURIST(List.of(SUNNY_SIDE_UP, FRIED_SAUSAGE, MASHED_POTATO, BUN, MUFFIN)),
     KID(List.of(PANCAKE, MUFFIN, CEREAL, MILK));
-
     private List<MealType> mealPreferences;
 
     GuestType(List<MealType> mealPreferences) {
