f7e462f3d1a3b26a7fc62c86d4337048e8d3e7da
Marton Benjamin Szak <105061239+martonszak@users.noreply.github.com>
2022-11-09 09:20:33 +0100

    fix the tests.

--- src/main/java/com/codecool/ehotel/service/buffet/BuffetService.java
+++ src/main/java/com/codecool/ehotel/service/buffet/BuffetService.java
@@ -2,12 +2,16 @@ package com.codecool.ehotel.service.buffet;
 
 import com.codecool.ehotel.model.Buffet;
 import com.codecool.ehotel.model.Meal;
+import com.codecool.ehotel.model.MealDurability;
 import com.codecool.ehotel.model.MealType;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public interface BuffetService {
     Buffet refill(Buffet buffet, List<Meal> meals);
 
     boolean consumeFreshest(Buffet buffet, MealType mealType);
+
+    int collectWaste(MealDurability mealDurability, LocalTime timeStamp); //Freshness wtf? :D
 }
